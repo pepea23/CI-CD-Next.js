ab92bba19d03e7521b066d21720285a2248e75bd
pepea <pepea.23@live.com>
2019-12-17 02:38:41 +0700

    fix useEffect

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,18 @@ const initializeLiff = myLiffId => {
     .then(() => {
       // start to use LIFF's api
       initializeApp();
+      liff.login();
     })
     .catch(err => {
-      document.getElementById("liffAppContent").classList.add("hidden");
-      document
-        .getElementById("liffInitErrorMessage")
-        .classList.remove("hidden");
+      console.log(err);
     });
 };
 
-useEffect(() => {
-  initializeLiff();
-}, []);
-
-const Home = () => <div>welcome to liff</div>;
+const Home = () => {
+  useEffect(() => {
+    initializeLiff();
+  }, []);
+  return <div>welcome to liff</div>;
+};
 
 export default Home;
